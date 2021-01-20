0f6f0e0501d37d3f3bb39829ea15d04672555bf7
Ellis Delgado <ellisdelgado13@gmail.com>
2021-01-20 14:13:11 -0800

    0.0.5 Added a start script and changed the port on server.js

--- Develop/server.js
+++ Develop/server.js
@@ -6,7 +6,7 @@ const { notStrictEqual } = require("assert");
 // Setting up the Express App
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: true }));
